87657670c4035447cc2d14a34acfc3d3baf05a65
bres <geobres@gmail.com>
2020-08-12 18:03:13 +0300

    create the crud

--- website/pages/urls.py
+++ website/pages/urls.py
@@ -4,12 +4,13 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('',views.home,name='home'),
-    path('about/',views.about,name='about'),
-    path('projects/',views.projects,name='projects'),
-    path('blog/',views.blog,name='blog'),
-    path('contact/',views.contact,name='contact'),
+    path('',views.home_nav,name='home_nav'),
+    path('about/',views.about_nav,name='about_nav'),
+    path('projects/',views.projects_nav,name='projects_nav'),
+    path('blog/',views.blog_nav,name='blog_nav'),
+    path('contact/',views.contact_nav,name='contact_nav'),
+    path('dashboard/',views.dashboard,name='dashboard'),
     path('create/',views.createPost,name='createPost'),
 
- 
+
 ]
--- website/pages/views.py
+++ website/pages/views.py
@@ -2,37 +2,36 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import *
 from .forms import PostForm
-# Create your views here.
-
-def home(request):
+######################################################################
+# nav views
+def home_nav(request):
     all_items=Landing_content.objects.all
     latest_post = Post.objects.last()
     return render(request,"home.html",{'all_items':all_items,'latest_post':latest_post})
 
-def about(request):
+def about_nav(request):
     return render(request,"about.html",{})
 
-def projects(request):
+def projects_nav(request):
     all_projects=Project.objects.all
     return render(request,"projects.html",{'all_projects':all_projects})
 
-def blog(request):
-    all_posts=Post.objects.order_by('-id') 
+def blog_nav(request):
+    all_posts=Post.objects.order_by('-id')
     return render(request,"blog.html",{'all_posts':all_posts})
 
-
-def contact(request):
+def contact_nav(request):
     return render(request,"contact.html",{})
- 
+
+######################################################################
+# dashboard views
 def createPost(request):
-    if request.method =="POST":
         form = PostForm(request.POST or None,request.FILES or None)
         if form.is_valid():
             form.save()
-            return redirect('createPost')
-    else:
-        form = PostForm()
+            return redirect('/')
+        return render(request,"newtask.html",{'form':form})
 
-    return render(request,"post_form.html",
-                 {'form':form}
-                 ) 
+def dashboard(request):
+    context=Post.objects.all
+    return render(request,"dashboard.html",{'context':context})
